ca3e862d656d08f84728e3b2fcf3429e62cbc891
Dmg5th <dmgould310@gmail.com>
2021-01-21 11:19:13 -0500

    commit tree f7db50bc303714dac4b255837e3f169ac2f5550f

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(array){
+    let newArray = []
+    let split = array.split(" ")
+   
+    split.forEach(word => {
+      if (word !== "the" && word !== "a" &&  word !== "an" &&  word !== "but" &&  word !== "of" && word !== "and" &&  word !== "for" &&  word !== "at" && word !== "by" &&  word !== "from") {
+        newArray.push(Formatter.capitalize(word))
+      } else {
+        newArray.push(word)
+      }
+    });
+    newArray.push(split[0].charAt(0).toUpperCase() + split[0].slice(1))
+     return newArray.join(" ")
+  }
+// static titleize(string) {
+//   let newArray = []
+//   let array = string.split(' ')
+//   newArray.push(array[0].charAt(0).toUpperCase() + array[0].slice(1))
+//   for (let i = 1; i < array.length; i++) {
+//     if (array[i] !== 'the' && array[i] !== 'a' && array[i] !== 'an' && array[i] !== 'but' && array[i] !== 'of' && array[i] !== 'and' && array[i] !== 'for' && array[i] !== 'at' && array[i] !== 'by' && array[i] !== 'from') {
+//       newArray.push(Formatter.capitalize(array[i]))
+//     } else {
+//       newArray.push(array[i])
+//     }
+//   }
+//   return newArray.join(' ')
+// }
+// }
 }
